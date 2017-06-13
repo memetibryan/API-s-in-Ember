ce4c0090e98eb7f3ceec3163c866b97aa7f1fc92
memetibryan <memetibryan@gmail.com>
2017-06-13 11:41:33 +0300

    included the APIkey in .env file and exported it.Created a variable that stores the api key value then insert it into the url mapper

--- app/routes/results.js
+++ app/routes/results.js
@@ -1,11 +1,12 @@
+import config from '../config/environment';
 import Ember from 'ember';
 
-
 export default Ember.Route.extend({
- model: function(params) {
-   var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=[YOUR_API_KEY_HERE]&zip=' + params.zip;
-   return Ember.$.getJSON(url).then(function(responseJSON) {
-     return responseJSON.results;
-   });
+  model: function(params) {
+    var key = config.myApiKey;
+    var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=' +key+ '&zip=' +params.zip;
+    return Ember.$.getJSON(url).then(function(responseJSON) {
+      return responseJSON.results;
+    });
   }
 });
